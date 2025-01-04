89face8cd7d025e883e4f987d0ab8cfb96e8ed43
rizqifauzy <rizqifauzy10@gmail.com>
2025-01-04 13:23:44 +0700

    add new

---
+++ refactor/vcenter-login-api/go.mod
@@ -0,0 +1,3 @@
+module github.com/rizqifauzy/golang-devops/refactor/vcenter-login-pkg
+
+go 1.22.4
---
+++ refactor/vcenter-login-pkg/api/init.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+	"crypto/tls"
+	"log"
+	"net/http"
+)
+
+type Options struct {
+	Username string
+	Password string
+	LoginURL string
+}
+
+type APIIface interface {
+	DoPowerRequest(requestURL string, action string) (Output, error)
+}
+
+type api struct {
+	Options Options
+	Client  http.Client
+}
+
+func New(options Options) APIIface {
+	if options.LoginURL == "" {
+		log.Fatal("LoginURL is required")
+	}
+
+	tlsConfig := &tls.Config{InsecureSkipVerify: true}
+	transport := &http.Transport{
+		TLSClientConfig: tlsConfig,
+	}
+
+	return api{
+		Options: options,
+		Client: http.Client{
+			Transport: &MyJWTTransport{
+				Transport: transport,
+				loginURL:  options.LoginURL,
+			},
+		},
+	}
+}
