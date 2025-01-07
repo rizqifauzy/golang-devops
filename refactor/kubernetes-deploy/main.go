bba6dbc5767ed68a1a7c04e411b5f93c4054a1d2
rizqifauzy <rizqifauzy10@gmail.com>
2025-01-07 12:27:53 +0700

    add new

--- refactor/kubernetes-deploy/main.go
+++ refactor/kubernetes-deploy/main.go
@@ -3,45 +3,45 @@ package main
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
-	"time"
 
 	v1 "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
 	var (
-		client           *kubernetes.Clientset
-		deploymentLabels map[string]string
-		err              error
-		expectedPods     int32
+		client *kubernetes.Clientset
+		err    error
 	)
 	ctx := context.Background()
+
 	if client, err = getClient(); err != nil {
-		fmt.Printf("Error: %s", err)
+		fmt.Printf("Get Client Error : %s", err)
 		os.Exit(1)
 	}
-	if deploymentLabels, expectedPods, err = deploy(ctx, client); err != nil {
-		fmt.Printf("Error: %s", err)
+	//fmt.Printf("Client: %+v\n", client)
+	if err = deploy(ctx, client); err != nil {
+		fmt.Printf("Deployment Error : %s", err)
 		os.Exit(1)
 	}
-	if err = waitForPods(ctx, client, deploymentLabels, expectedPods); err != nil {
-		fmt.Printf("Error: %s", err)
-		os.Exit(1)
-	}
-	fmt.Printf("deploy finished. Did a deploy with labels: %+v\n", deploymentLabels)
+	fmt.Printf("Deploy finished\n")
+
 }
 
 func getClient() (*kubernetes.Clientset, error) {
+	//var kubeconfig *string
+	//if home := homedir.HomeDir(); home != "" {
+	//kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	// } else {
+	// 	kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	// }
+	//flag.Parse()
+
 	// use the current context in kubeconfig
 	config, err := clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
 	if err != nil {
@@ -53,74 +53,15 @@ func getClient() (*kubernetes.Clientset, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	return clientset, nil
 }
 
-func deploy(ctx context.Context, client *kubernetes.Clientset) (map[string]string, int32, error) {
+func deploy(ctx context.Context, client *kubernetes.Clientset) (map[string]string, error) {
 	var deployment *v1.Deployment
-
-	appFile, err := ioutil.ReadFile("app.yaml")
+	deploymentResponse, err := client.AppsV1().Deployments("default").Create(ctx, deployment, metav1.CreateOptions{})
 	if err != nil {
-		return nil, 0, fmt.Errorf("readfile error: %s", err)
+		return nil, fmt.Errorf("deployment error: %s", err)
 	}
-
-	obj, groupVersionKind, err := scheme.Codecs.UniversalDeserializer().Decode(appFile, nil, nil)
-	if err != nil {
-		return nil, 0, fmt.Errorf("Decode error: %s", err)
-	}
-
-	switch obj.(type) {
-	case *v1.Deployment:
-		deployment = obj.(*v1.Deployment)
-	default:
-		return nil, 0, fmt.Errorf("Unrecognized type: %s\n", groupVersionKind)
-	}
-
-	_, err = client.AppsV1().Deployments("default").Get(ctx, deployment.Name, metav1.GetOptions{})
-	if err != nil && errors.IsNotFound(err) {
-		deploymentResponse, err := client.AppsV1().Deployments("default").Create(ctx, deployment, metav1.CreateOptions{})
-		if err != nil {
-			return nil, 0, fmt.Errorf("deployment error: %s", err)
-		}
-		return deploymentResponse.Spec.Template.Labels, *deploymentResponse.Spec.Replicas, nil
-	} else if err != nil && !errors.IsNotFound(err) {
-		return nil, 0, fmt.Errorf("deployment get error: %s", err)
-	}
-
-	deploymentResponse, err := client.AppsV1().Deployments("default").Update(ctx, deployment, metav1.UpdateOptions{})
-	if err != nil {
-		return nil, 0, fmt.Errorf("deployment error: %s", err)
-	}
-	return deploymentResponse.Spec.Template.Labels, *deploymentResponse.Spec.Replicas, nil
-
-}
-func waitForPods(ctx context.Context, client *kubernetes.Clientset, deploymentLabels map[string]string, expectedPods int32) error {
-	for {
-		validatedLabels, err := labels.ValidatedSelectorFromSet(deploymentLabels)
-		if err != nil {
-			return fmt.Errorf("ValidatedSelectorFromSet error: %s", err)
-		}
-
-		podList, err := client.CoreV1().Pods("default").List(ctx, metav1.ListOptions{
-			LabelSelector: validatedLabels.String(),
-		})
-		if err != nil {
-			return fmt.Errorf("pod list error: %s", err)
-		}
-		podsRunning := 0
-		for _, pod := range podList.Items {
-			if pod.Status.Phase == "Running" {
-				podsRunning++
-			}
-		}
-
-		fmt.Printf("Waiting for pods to become ready (running %d / %d)\n", podsRunning, len(podList.Items))
-
-		if podsRunning > 0 && podsRunning == len(podList.Items) && podsRunning == int(expectedPods) {
-			break
-		}
-
-		time.Sleep(5 * time.Second)
-	}
-	return nil
+	return deploymentResponse.Spec.Template.Labels, nil
 }
--- refactor/vcenter-login-api/go.mod
+++ refactor/vcenter-login-api/go.mod
@@ -1,3 +1,5 @@
+//module github.com/rizqifauzy/golang-devops/refactor/vcenter-login-api
+//module github.com/rizqifauzy/golang-devops
 module github.com/rizqifauzy/golang-devops/refactor/vcenter-login-api
 
-go 1.22.4
+go 1.23.4
