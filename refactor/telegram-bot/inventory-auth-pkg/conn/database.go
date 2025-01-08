9f1c163c8ce8283aa5e85674d597aabbea5cf8e3
rizqifauzy <rizqifauzy10@gmail.com>
2025-01-09 00:09:43 +0700

    add new

--- refactor/telegram-bot/inventory-auth-pkg/conn/database.go
+++ refactor/telegram-bot/inventory-auth-pkg/conn/database.go
@@ -12,7 +12,7 @@ func DbConn() (*sql.DB, error) {
 	pg_password := config.Config("PG_PASSWORD")
 	pg_dbname := config.Config("PG_DBNAME")
 	pg_port := config.Config("PG_PORT")
-	pg_sslmode := config.Config("PG_SSLMODE")
+	pg_sslmode := config.Config("PG_SSL_MODE")
 
 	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", pg_host, pg_user, pg_password, pg_dbname, pg_port, pg_sslmode)
 	db, err := sql.Open("postgres", connStr)
